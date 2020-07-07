6fffdb42b198d67f0d7d5428030845f5711d52b3
moxiaolong <975425198@qq.com>
2020-07-07 16:07:08 +0800

    添加自动推送

--- baiduPush.sh
+++ baiduPush.sh
@@ -4,6 +4,6 @@
 set -e
 
 # 百度链接推送
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://xugaoyi.com&token=T5PEAzhGaPNbjQ2X"
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://dra-m.com&token=52OsvJCqQ9id4Har"
 
 rm -rf urls.txt # 灭迹
--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,19 @@
 #!/usr/bin/env sh
-
 # 确保脚本抛出遇到的错误
 set -e
-
-# 生成静态文件
-npm run build
-
-# 进入生成的文件夹
-cd docs/.vuepress/dist
+npm run build          # 生成静态文件
+cd docs/.vuepress/dist # 进入生成的文件夹
 
 # deploy to github
-echo 'b.xugaoyi.com' > CNAME
+echo 'dra-m.com' >CNAME
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
-  githubUrl=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+  githubUrl=git@github.com:moxiaolong/moxiaolong.github.io.git
 else
-  msg='来自github actions的自动部署'
-  githubUrl=https://xugaoyi:${GITHUB_TOKEN}@github.com/xugaoyi/vuepress-theme-vdoing.git
-  git config --global user.name "xugaoyi"
-  git config --global user.email "894072666@qq.com"
+  msg='来自github action的自动部署'
+  githubUrl=https://moxiaolong:${GITHUB_TOKEN}@github.com/xugaoyi/moxiaolong.github.io.git
+  git config --global user.name "moxiaolong"
+  git config --global user.email "975425198@qq.com"
 fi
 git init
 git add -A
@@ -26,15 +21,15 @@ git commit -m "${msg}"
 git push -f $githubUrl master:gh-pages # 推送到github
 
 # deploy to coding
-echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
-if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-  codingUrl=git@e.coding.net:xgy/xgy.git
+echo 'dra-m.com' >CNAME # 自定义域名
+if [ -z "$CODING_TOKEN" ]; then # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+  codingUrl=git@e.coding.net:moxiaolong/dram/dram.git
 else
-  codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
+  codingUrl=https://moxiaolong:${CODING_TOKEN}@e.coding.net:moxiaolong/dram/dram.git
 fi
 git add -A
 git commit -m "${msg}"
 git push -f $codingUrl master # 推送到coding
 
-cd - # 退回开始所在目录
+cd -
 rm -rf docs/.vuepress/dist
