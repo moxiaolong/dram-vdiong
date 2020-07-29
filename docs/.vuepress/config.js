004532de65dc08532512576b51dc174296ba97f6
moxiaolong <975425198@qq.com>
2020-07-29 10:18:20 +0800

    测试cdn

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -296,4 +296,33 @@ module.exports = {
 //     }
 //   }
 // }
+
+    configureWebpack: () => {
+        const NODE_ENV = process.env.NODE_ENV
+        //判断是否是生产环境
+        if (NODE_ENV === 'production') {
+            return {
+                output: {
+                    publicPath: 'https://cdn.jsdelivr.net/gh/moxiaolong/moxiaolong.github.io/'
+                },
+                resolve: {
+                    //配置路径别名
+                    alias: {
+                        'public': path.resolve(__dirname, './public')
+                    }
+                }
+            }
+        } else {
+            return {
+                resolve: {
+                    //配置路径别名
+                    alias: {
+                        'public': path.resolve(__dirname, './public')
+                    }
+                }
+            }
+        }
+    }
+
+
 }
