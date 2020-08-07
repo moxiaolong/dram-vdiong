5fa6ae47381e89b31093cfb096f1c83fa733723f
moxiaolong <975425198@qq.com>
2020-08-07 09:24:38 +0800

    添加内容

--- docs/.vuepress/public/live2d/waifu-tips.js
+++ docs/.vuepress/public/live2d/waifu-tips.js
@@ -115,16 +115,16 @@ function loadWidget(config) {
             else if (now > 19 && now <= 21) text = "晚上好，今天过得怎么样？";
             else if (now > 21 && now <= 23) text = ["已经这么晚了呀，早点休息吧，晚安～", "深夜时要爱护眼睛呀！"];
             else text = "你是夜猫子呀？这么晚还不睡觉，明天起的来嘛？";
-        } else if (t.referrer !== "") {
-            let referrer = new URL(t.referrer),
+        } else if (document.referrer !== "") {
+            let referrer = new URL(document.referrer),
                 domain = referrer.hostname.split(".")[1];
-            if (location.hostname === referrer.hostname) text = `欢迎阅读<span>「${t.title.split(" - ")[0]}」</span>`;
+            if (location.hostname === referrer.hostname) text = `欢迎阅读<span>「${document.title.split(" - ")[0]}」</span>`;
             else if (domain === "baidu") text = `Hello！来自 百度搜索 的朋友<br>你是搜索 <span>${referrer.search.split("&wd=")[1].split("&")[0]}</span> 找到的我吗？`;
             else if (domain === "so") text = `Hello！来自 360搜索 的朋友<br>你是搜索 <span>${referrer.search.split("&q=")[1].split("&")[0]}</span> 找到的我吗？`;
-            else if (domain === "google") text = `Hello！来自 谷歌搜索 的朋友<br>欢迎阅读<span>「${t.title.split(" - ")[0]}」</span>`;
+            else if (domain === "google") text = `Hello！来自 谷歌搜索 的朋友<br>欢迎阅读<span>「${document.title.split(" - ")[0]}」</span>`;
             else text = `Hello！来自 <span>${referrer.hostname}</span> 的朋友`;
         } else {
-            text = `欢迎阅读<span>「${t.title.split(" - ")[0]}」</span>`;
+            text = `欢迎阅读<span>「${document.title.split(" - ")[0]}」</span>`;
         }
         showMessage(text, 7000, 8);
     })();
