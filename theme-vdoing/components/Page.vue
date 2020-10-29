bb5a3ffc445faab51761a9d958f23b1361a7a250
moxiaolong <975425198@qq.com>
2020-10-29 16:02:43 +0800

    更新

--- theme-vdoing/components/Page.vue
+++ theme-vdoing/components/Page.vue
@@ -30,7 +30,7 @@
       />
 
       <!-- 广告 -->
-      <div style="padding:0;" class="ad">
+      <div style="padding:0;max-width: 900px;" class="ad">
         <ins class="adsbygoogle"
              style="display:block"
              data-ad-client="ca-pub-8621788234752924"
