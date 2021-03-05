6b984a7cd0e95234ea9205b40d6e29ba93d1fb7e
moxiaolong <975425198@qq.com>
2021-03-05 18:02:11 +0800

    修改目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -46,15 +46,15 @@ module.exports = {
 
     themeConfig: { // 主题配置
         htmlModules: {
-            // homeSidebarB: "<ins class=\"adsbygoogle\"\n" +
-            //     "     style=\"display:block\"\n" +
-            //     "     data-ad-client=\"ca-pub-8621788234752924\"\n" +
-            //     "     data-ad-slot=\"9814056513\"\n" +
-            //     "     data-ad-format=\"auto\"\n" +
-            //     "     data-full-width-responsive=\"true\"></ins>\n" +
-            //     "<script>\n" +
-            //     "     (adsbygoogle = window.adsbygoogle || []).push({});\n" +
-            //     "</script>", // 首页侧边栏底部
+            homeSidebarB: "<ins class=\"adsbygoogle\"\n" +
+                "     style=\"display:block\"\n" +
+                "     data-ad-client=\"ca-pub-8621788234752924\"\n" +
+                "     data-ad-slot=\"9814056513\"\n" +
+                "     data-ad-format=\"auto\"\n" +
+                "     data-full-width-responsive=\"true\"></ins>\n" +
+                "<script>\n" +
+                "     (adsbygoogle = window.adsbygoogle || []).push({});\n" +
+                "</script>", // 首页侧边栏底部
             //  sidebarT: htmlString, //全局左侧边栏顶部
             sidebarB: "<ins class=\"adsbygoogle\"\n" +
                 "     style=\"display:block\"\n" +
@@ -91,7 +91,7 @@ module.exports = {
 
         // 以下配置是Vdoing主题改动的和新增的配置
         // category: false, // 是否打开分类功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含分类字段 2.页面中显示与分类相关的信息和模块 3.自动生成分类页面（在@pages文件夹）。如关闭，则反之。
-        // tag: false, // 是否打开标签功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含标签字段 2.页面中显示与标签相关的信息和模块 3.自动生成标签页面（在@pages文件夹）。如关闭，则反之。
+        tag: false, // 是否打开标签功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含标签字段 2.页面中显示与标签相关的信息和模块 3.自动生成标签页面（在@pages文件夹）。如关闭，则反之。
         // archive: false, // 是否打开归档功能，默认true。 如打开，会做的事情有：1.自动生成归档页面（在@pages文件夹）。如关闭，则反之。
         // categoryText: '随笔', // 碎片化文章（_posts文件夹的文章）预设生成的分类值，默认'随笔'
         // bodyBgImg: [
