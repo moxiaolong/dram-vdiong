2ee31036f9146124de3d1ecea14da79efbd5cdae
DragonMo <975425198@qq.com>
2022-04-11 22:08:38 +0800

    关闭编辑按钮

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -56,7 +56,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         searchMaxSuggestions: 10, // 搜索结果显示最大数
         lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
         docsDir: 'docs', // 编辑的文件夹
-        editLinks: true, // 启用编辑
+        editLinks: false, // 启用编辑
         editLinkText: '编辑',
 
         //*** 以下是Vdoing主题相关配置，文档：https://doc.xugaoyi.com/pages/a20ce8/ ***//
