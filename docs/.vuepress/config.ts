360448f3a80bffa05b86b1650da2ef7adb26b2e4
dragon <975425198@qq.com>
2023-09-26 11:09:21 +0800

    更新配置

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -8,6 +8,7 @@ import {VdoingThemeConfig} from 'vuepress-theme-vdoing/types'
 import dayjs from 'dayjs'
 import baiduCode from './config/baiduCode' // 百度统计hm码
 import htmlModules from './config/htmlModules' // 自定义插入的html块
+const DOMAIN_NAME = 'dra-m.com' // 域名 (不带https)
 
 export default defineConfig4CustomTheme<VdoingThemeConfig>({
     theme: 'vdoing', // 使用npm包主题
@@ -17,7 +18,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         '/': {
             lang: 'zh-CN',
             title: "Dra-M",
-            description: '一些技术(前端、后端、运维)相关的经验、随想、资源收藏，和一些哲学相关随想。',
+            description: '一些技术(前端、后端、运维)相关的经验、随想、资源收藏，和一些哲学随想。',
         }
     },
     // base: '/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
@@ -169,15 +170,15 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         //     href: '//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
         // }], //favicons，资源放在public文件夹
 
-        ['meta', {name: 'keywords', content: '后端技术,DragonMo,Dra-M,莫小龙'}],
+        ['meta', {name: 'keywords', content: '开发技术,哲学,DragonMo,Dra-M,莫小龙'}],
         // ['meta', {name: 'baidu-site-verification', content: '7F55weZDDc'}],// 百度统计博主验证
         ['meta', {name: 'theme-color', content: '#7b074b'}], // 移动浏览器主题颜色
-        ['script', {
-            'data-ad-client': 'ca-pub-8621788234752924',
-            defer: true,
-            src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8621788234752924',
-            crossorigin: "anonymous"
-        }] // 移动浏览器主题颜色
+        // ['script', {
+        //     'data-ad-client': 'ca-pub-8621788234752924',
+        //     defer: true,
+        //     src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8621788234752924',
+        //     crossorigin: "anonymous"
+
 
     ],
 
@@ -195,34 +196,20 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         // ],
 
         // 全文搜索。 ⚠️注意：此插件会在打开网站时多加载部分js文件用于搜索，导致初次访问网站变慢。如在意初次访问速度的话可以不使用此插件！（推荐：vuepress-plugin-thirdparty-search）
-        'fulltext-search',
+        // 'fulltext-search',
 
         // 可以添加第三方搜索链接的搜索框（继承原官方搜索框的配置参数）
-        // 'thirdparty-search': {
-        //   thirdparty: [
-        //     {
-        //       title: '在MDN中搜索',
-        //       frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
-        //       behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
-        //     },
-        //     {
-        //       title: '在Runoob中搜索',
-        //       frontUrl: 'https://www.runoob.com/?s=',
-        //     },
-        //     {
-        //       title: '在Vue API中搜索',
-        //       frontUrl: 'https://cn.vuejs.org/v2/api/#',
-        //     },
-        //     {
-        //       title: '在Bing中搜索',
-        //       frontUrl: 'https://cn.bing.com/search?q=',
-        //     },
-        //     {
-        //       title: '通过百度搜索本站的',
-        //       frontUrl: 'https://www.baidu.com/s?wd=site%3Axugaoyi.com%20',
-        //     },
-        //   ],
-        // },
+        [
+            'thirdparty-search',
+            {
+                thirdparty: [
+                    {
+                        title: '通过百度搜索本站的',
+                        frontUrl: `https://www.baidu.com/s?wd=site%3A${DOMAIN_NAME}%20`,
+                    },
+                ],
+            }
+        ],
 
         [
             'one-click-copy', // 代码块复制按钮
